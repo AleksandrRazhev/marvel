d85ec2fe6dedc79f73a0110cad37d37c85ed8fa5
AleksandrRazhev <alexandrrazh@gmail.com>
2022-02-21 16:00:30 +0300

    layout added

--- src/components/CharList/CharList.js
+++ src/components/CharList/CharList.js
@@ -1,7 +1,53 @@
+import './CharList.scss';
+
+import abyss from '../../resources/img/abyss.jpg';
+
 const CharList = () => {
   return (
-    <div className="">
-      CharList
+    <div className="char__list">
+      <ul className="char__grid">
+        <li className="char__item">
+          <img src={abyss} alt="Abyss" />
+          <div className="char__name">Abyss</div>
+        </li>
+        <li className="char__item char__item_selected">
+          <img src={abyss} alt="Abyss" />
+          <div className="char__name">Abyss</div>
+        </li>
+        <li className="char__item">
+          <img src={abyss} alt="Abyss" />
+          <div className="char__name">Abyss</div>
+        </li>
+        <li className="char__item">
+          <img src={abyss} alt="Abyss" />
+          <div className="char__name">Abyss</div>
+        </li>
+        <li className="char__item">
+          <img src={abyss} alt="Abyss" />
+          <div className="char__name">Abyss</div>
+        </li>
+        <li className="char__item">
+          <img src={abyss} alt="Abyss" />
+          <div className="char__name">Abyss</div>
+        </li>
+        <li className="char__item">
+          <img src={abyss} alt="Abyss" />
+          <div className="char__name">Abyss</div>
+        </li>
+        <li className="char__item">
+          <img src={abyss} alt="Abyss" />
+          <div className="char__name">Abyss</div>
+        </li>
+        <li className="char__item">
+          <img src={abyss} alt="Abyss" />
+          <div className="char__name">Abyss</div>
+        </li>
+      </ul>
+      <div className="char__button">
+        <div className="button button__main button__long">
+          <div className="inner">load more</div>
+        </div>
+      </div>
     </div>
   )
 };
--- src/components/RandomChar/RandomChar.js
+++ src/components/RandomChar/RandomChar.js
@@ -1,6 +1,8 @@
+import '../../style/buttons.scss';
 import './RandomChar.scss';
 
 import thor from '../../resources/img/thor.jpeg';
+import mjolnir from '../../resources/img/mjolnir.png';
 
 const RandomChar = () => {
   return (
@@ -12,18 +14,33 @@ const RandomChar = () => {
           <p className="randomchar__descr">As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...</p>
           <div className="randomchar__btns">
             <a href="#" className="button button__main">
-              <div className="inner">homepage</div>
+              <p className="inner">homepage</p>
             </a>
             <a href="#" className="button button__secondary">
-              <div className="inner">Wiki</div>
+              <p className="inner">Wiki</p>
             </a>
           </div>
         </div>
       </div>
       <div className="randomchar__static">
-        <p>
-          Random character for today! Do you want to get to know him better?
+        <p className="randomchar__title">
+          Random character for today! <br />
+          Do you want to get to know him better?
         </p>
+        <p className="randomchar__title">
+          Or choose another one
+        </p>
+        <button className="button button__main">
+          <p className="inner">try it</p>
+        </button>
+        <span style={
+          {
+            backgroundImage: `url(${mjolnir})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: '35px -14px',
+            backgroundSize: 'cover',
+          }
+        } className="randomchar__decoration"></span>
       </div>
     </div>
   )
